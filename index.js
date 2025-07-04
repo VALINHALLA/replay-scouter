8efc7a28ecc167f2fdaa94b9d5907b8e089a4c2b
VALINHALLA <gentletrashyyy@gmail.com>
2025-07-04 17:51:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+// src/index.ts
+export default {
+  async fetch(request: Request): Promise<Response> {
+    return new Response("Hello from Worker!", {
+      headers: { "content-type": "text/plain" },
+    });
+  }
+};
